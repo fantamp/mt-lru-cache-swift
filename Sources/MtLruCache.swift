66d7c8f0b91c7ed541ca2ca25ebd9ba9c1382cc7
Andrey Mescheryakov <fantamp@yandex-team.ru>
2017-07-17 22:03:29 +0300

    Move DLL in a separate file; Make Cache public

--- Sources/MtLruCache.swift
+++ Sources/MtLruCache.swift
@@ -9,72 +9,7 @@
 import Foundation
 
 
-
-class DoubleLinkedList {
-    class Node {
-        var prev: Node? = nil
-        var next: Node? = nil
-        var key: String
-
-        init(key: String) {
-            self.key = key
-        }
-    }
-
-    var head: Node?
-    var tail: Node?
-    var size: UInt = 0
-
-    func insertHead(_ n: Node) {
-        n.next = head
-        n.prev = nil
-        if let h = head {
-            h.prev = n
-        } else {
-            tail = n
-        }
-        head = n
-        size += 1
-    }
-
-    func insertHead(key: String) -> Node {
-        let n = Node(key: key)
-        insertHead(n)
-        return n
-    }
-
-    func moveToHead(_ n: Node) {
-        remove(n)
-        insertHead(n)
-    }
-
-    func removeTail() {
-        if let t = tail {
-            remove(t)
-        }
-    }
-
-    func remove(_ n: Node) {
-        if let prev = n.prev {
-            prev.next = n.next
-        }
-        if let next = n.next {
-            next.prev = n.prev
-        }
-        // self.tail and self.head are guarnateed to be not nil here:
-        // If someone calls remove(n) that means that `n` is in the list. Which
-        // means that both self.head and self.tails are not nil
-        if n === tail! {
-            tail = n.prev
-        }
-        if n === head! {
-            head = n.next
-        }
-        size -= 1
-    }
-}
-
-class Cache {
+public class Cache {
     private class OperationInProgress {
         private var payload: AnyObject? = nil
         private var cond: NSCondition = NSCondition()
